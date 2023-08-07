f31dc050aedbb53e1f0fa8df8f9478a4c5df5e7b
BoniK <mail@bonik.me>
2023-08-07 18:49:27 +0900

    1.0

--- Ghost_Webhook.py
+++ Ghost_Webhook.py
@@ -1,29 +1,111 @@
 # abcd.com/webhookreq에 요청 들어온 그대로 콘솔에 출력
-import os
+import os, sys
+import configparser
+import requests
+import socket
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+#info
+Ghost_Webhook_version = 1.0
+Ghost_Webhook_path = os.path.dirname(os.path.realpath(__file__))
+
+
+# 내부 아이피 확인 
+# ini파일에서 정보 불러오기
+config = configparser.ConfigParser()
+config.read(f'{Ghost_Webhook_path}/Ghost_Webhook.ini', encoding='UTF8')
+settings = config['SETTINGS']
+IP_AUTO_DETECT = settings["IP_AUTO_DETECT"] 
+if IP_AUTO_DETECT == 'Y':
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('192.255.255.255', 1))
+    HOST_IP = s.getsockname()[0]
+    s.close()
+
+    # ini파일에 내부 아이피 작성
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    config.read(f'{Ghost_Webhook_path}/Ghost_Webhook.ini', encoding='UTF8')
+    with open(f'{Ghost_Webhook_path}/Ghost_Webhook.ini', "w", encoding='UTF8') as f:
+        config.set('SETTINGS', "GHOST_WEBHOOK_IP", f"{HOST_IP}")    
+        config.write(f)
+
+
+# ini파일에서 정보 불러오기
+config = configparser.ConfigParser()
+config.read(f'{Ghost_Webhook_path}/Ghost_Webhook.ini', encoding='UTF8')
+settings = config['SETTINGS']
+IP_AUTO_DETECT = settings["IP_AUTO_DETECT"] 
+GHOST_WEBHOOK_IP = settings["GHOST_WEBHOOK_IP"]
+GHOST_WEBHOOK_PORT = settings["GHOST_WEBHOOK_PORT"]
+TELEGRAM = settings["TELEGRAM"]
+TELEGRAM_TOKEN = settings["TELEGRAM_TOKEN"]
+CHAT_ID = settings["CHAT_ID"]
+DISCORD = settings["DISCORD"]
+WEBHOOKURL = settings["WEBHOOKURL"]
+USERNAME = settings["USERNAME"]
+AVATAR_URL = settings["AVATAR_URL"]
+
+
+# 텔래그램 전송
+def send_telegram(message):    
+    if TELEGRAM == 'Y':
+        base_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+        data = {"chat_id": CHAT_ID, "text": message}
+        result = requests.post(base_url, data=data)
+        return result.status_code
+    else:
+        return
+
+
+# 디스코드 전송
+def send_discord(message):
+    if DISCORD == 'Y':
+        data = {
+    "content": message,
+    "username": USERNAME,
+    "avatar_url": AVATAR_URL,
+    }
+        result = requests.post(WEBHOOKURL, json=data)
+        return result.status_code
+    else:
+        return
+
+
+# Ghost의 Post Published 웹훅
 @app.route('/post_published', methods=['POST'])
 def post_published():
     if request.method == 'POST':
-        data = request.get_data(as_text=True)
-        print(data)
-        return f"Received message: {data}"
-    return "Invalid method."
+        try:
+            data = request.get_json()  # JSON 데이터를 딕셔너리로 변환
+            post_url = data["post"]["current"]["url"]  # 받은 JSON 데이터를 딕셔너리로 출력            
+            # 텔레그램, 디스코드 전송
+            if not post_url == '':
+                send_telegram(post_url)
+                send_discord(post_url)
+        except Exception as e:
+            return e, 400
+    return ""
 
+
+# Ghost의 Post Updated 웹훅
 @app.route('/post_updated', methods=['POST'])
 def post_updated():
     if request.method == 'POST':
         try:
             data = request.get_json()  # JSON 데이터를 딕셔너리로 변환
-            post_url = data["post"]["current"]["url"]  # 받은 JSON 데이터를 딕셔너리로 출력
-            print(post_url)
-            # 텔레그램, 디스코드 전송 os.system(f'sendtele.exe "포스트 업데이트 {post_url}"')
-            return ""
+            post_url = data["post"]["current"]["url"]  # 받은 JSON 데이터를 딕셔너리로 출력            
+            # 텔레그램, 디스코드 전송
+            if not post_url == '':
+                send_telegram(post_url)
+                send_discord(post_url)            
         except Exception as e:
-            return jsonify({"error": "Invalid JSON data", "details": str(e)}), 400
+            return e, 400
     return ""
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=GHOST_WEBHOOK_PORT, debug=True, threaded=True)
--- README.md
+++ README.md
@@ -1,7 +1,36 @@
-# Ghost_Webhook
-고스트의 Webhook 신호를 받아서 원하는 명령을 처리하는 Flask 서버
+# 스크립트 설명
+고스트의 Webhook을 이용한 어플입니다.
+고스트에서 새글이 발행되거나 기존 글이 업데이트 되었을 때, 텔레그램 및 디스코드로 알립니다.
 
-# pip
+# 파이썬 모듈 설치
 ```
-pip install Flask[async]
+pip install -r requirements.txt
 ```
+
+# 스크립트가 실행되는 서버의 IP와 포트 확인
+- 포트는 flask의 기본 포트인 5000번(ini파일에서 변경가능)
+- 외부 아이피나 특수한 네트워크 일 경우 ini파일의 IP_AUTO_DETECT값을 N으로 적고, 아이피 직접 입력 (e.g: 123.123.123.123)
+
+# 텔레그램 및 디스코드 설정
+- 텔레그램 토큰 및 챗아이디 생성(인터넷 검색)
+- 디스코드 WEBHOOKURL 생성(인터넷 검색)
+
+# 고스트 웹훅 설정
+## 웹훅 설정으로 이동
+- 고스트 관리자 - Integrations - CUSTOM INTEGRATIONS - Configure
+## 웹훅 설정
+- 새 글 알림만 이용하고 싶으면 Post Published만 설정하면 됩니다.
+### Post Published
+- 새 글 발행시 발동되는 웹훅
+- Event : Post published 선택
+- Target URL : 본인의 스크립트 실행 서버 IP를 사용해서 `http://123.123.123.123:5000/post_published`와 같은 형태로 입력
+### Post Updated
+- 글 업데이트시 발동되는 웹훅
+- Event : Post updated 선택
+- Target URL : 본인의 스크립트 실행 서버 IP를 사용해서 `http://123.123.123.123:5000/post_updated`와 같은 형태로 입력
+
+
+# Ghost_Webhook.ini 파일 작성
+- Ghost_Webhook-example.ini 복사 후 Ghost_Webhook.ini로 파일명 변경
+- 텔레그램, 디스코드 기능 사용여부 작성 (Y 또는 N)
+- 디스코드 유저명과 아바타 이미지는 원하는 대로 작성
